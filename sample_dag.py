4939ff11d67a73d01d4223be1bab648b20d0c4b4
HarryRichard08 <75106918+HarryRichard08@users.noreply.github.com>
2023-12-29 19:00:01 +0530

    Update sample_dag.py

--- sample_dag.py
+++ sample_dag.py
@@ -1,35 +1,28 @@
-# Importing necessary libraries
 from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
-
-# Define the Python function
-def print_hello():
-    print("Hello, World!")
+from airflow.operators.bash_operator import BashOperator
 
 # Default arguments for the DAG
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
+    'start_date': datetime(2023, 1, 1),  # Adjust the start date to your needs
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
 
-# Instantiate a DAG
-dag = DAG('hello_world_dag',
-          description='Simple tutorial DAG',
-          schedule_interval=timedelta(days=1),  # This DAG is set to run daily
-          start_date=datetime(2023, 8, 30),
-          catchup=False,
-          default_args=default_args)
+# Create a DAG instance
+dag = DAG('run_book_spider',
+          default_args=default_args,
+          description='Run Scrapy Spider with BashOperator',
+          schedule_interval=timedelta(days=1),  # Adjust the schedule to your needs
+          catchup=False)  # Set to False if you don't want to perform catchup (backfilling past dates)
 
-# Set up a task using the PythonOperator
-hello_task = PythonOperator(task_id='print_hello_world',
-                            python_callable=print_hello,
-                            dag=dag)
-
-# Set the task order (in this case, only one task, so this is optional)
-hello_task
-
+# Task to run the Scrapy spider
+run_spider_task = BashOperator(
+    task_id='run_scrapy_spider',
+    # Ensure the command matches the path and name of your Scrapy project and spider
+    bash_command='cd /root/projects/Scrapy-template/books && scrapy crawl book -o /root/output_files/book.csv',
+    dag=dag)
