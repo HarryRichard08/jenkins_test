33d9761be77568c9a52907c59f8ff68ded38e710
HarryRichard08 <75106918+HarryRichard08@users.noreply.github.com>
2024-01-04 16:09:10 +0530

    Update book_spider.py

--- Scrapy-template/books/spiders/book_spider.py
+++ Scrapy-template/books/spiders/book_spider.py
@@ -6,19 +6,19 @@ class BookSpider(scrapy.Spider):
     name = 'book'
     allowed_domains = ['books.toscrape.com']
     start_urls = ['http://amazon.com']
-
-    def parse(self, response):
-        # Select all the book containers from the page
-        books = response.css('article.product_pod')
-
-        # Iterate through each book and extract details
-        for book in books:
-            loader = ItemLoader(item=BookItem(), selector=book, response=response)
-            
-            # Use CSS selectors to extract the data
-            loader.add_css('title', 'h8 a::attr(title)')
-            loader.add_css('price', 'p.price_color::text')
-            loader.add_css('availability', 'p.instock.availability::text')
+    
+        def parse(self, response):
+            # Select all the book containers from the page
+            books = response.css('article.product_pod')
+    
+            # Iterate through each book and extract details
+            for book in books:
+                loader = ItemLoader(item=BookItem(), selector=book, response=response)
+                
+                # Use CSS selectors to extract the data
+                loader.add_css('title', 'h8 a::attr(title)')
+                loader.add_css('price', 'p.price_color::text')
+                loader.add_css('availability', 'p.instock.availability::text')
 
             yield loader.load_item()
 
